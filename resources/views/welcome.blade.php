e50e4e43590f73bd4be7fecd64e047cf5fb408a3
fredimaala <97944137+fredimaala@users.noreply.github.com>
2023-12-05 23:53:28 +0200

    katki

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,9 @@
 @extends('partials.layout')
-@section('title', 'Home page')
+@section('title', 'Dashboard page')
 @section('content')
     <div class="container mx-auto">
         <div class="flex flex-row flex-wrap">
-            @foreach ($posts as $post)
+            @foreach($posts as $post)
                 <div class="basis-1/4 my-3">
                     <div class="card bg-base-200 shadow-xl min-h-full m-2">
                         {{-- <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure> --}}
